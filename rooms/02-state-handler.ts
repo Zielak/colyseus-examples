f345805b226cefac8b8b9bae248602ab4ac2d8c9
Darek Zieliński <dariusz.zielinski@dazn.com>
2020-11-12 22:20:36 +0100

    ArraySchema push/splice/push issue

--- rooms/02-state-handler.ts
+++ rooms/02-state-handler.ts
@@ -1,5 +1,5 @@
 import { Room, Client } from "colyseus";
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, type, ArraySchema, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
     @type("number")
@@ -12,6 +12,8 @@ export class Player extends Schema {
 export class State extends Schema {
     @type({ map: Player })
     players = new MapSchema<Player>();
+    @type(["number"])
+    arrayTest = new ArraySchema<number>();
 
     something = "This attribute won't be sent to the client-side";
 
@@ -23,27 +25,49 @@ export class State extends Schema {
         this.players.delete(sessionId);
     }
 
-    movePlayer (sessionId: string, movement: any) {
+    movePlayer(sessionId: string, movement: any) {
         if (movement.x) {
             this.players.get(sessionId).x += movement.x * 10;
-
         } else if (movement.y) {
             this.players.get(sessionId).y += movement.y * 10;
         }
+    }
+
+    addNumber(value: number) {
+        this.arrayTest.push(value);
+    }
+
+    removeNumber(index: number) {
+        this.arrayTest.splice(index, 1);
     }
 }
 
 export class StateHandlerRoom extends Room<State> {
     maxClients = 4;
 
-    onCreate (options) {
+    onCreate(options) {
         console.log("StateHandlerRoom created!", options);
 
         this.setState(new State());
 
         this.onMessage("move", (client, data) => {
-            console.log("StateHandlerRoom received message from", client.sessionId, ":", data);
+            console.log(
+                "StateHandlerRoom received message from",
+                client.sessionId,
+                ":",
+                data
+            );
             this.state.movePlayer(client.sessionId, data);
+        });
+
+        this.onMessage("addNumber", (client, data) => {
+            console.log("Adding number:", data);
+            this.state.addNumber(data);
+        });
+
+        this.onMessage("removeNumber", (client, data) => {
+            console.log("Removing number from index:", data);
+            this.state.removeNumber(data);
         });
     }
 
@@ -51,17 +75,16 @@ export class StateHandlerRoom extends Room<State> {
         return true;
     }
 
-    onJoin (client: Client) {
+    onJoin(client: Client) {
         client.send("hello", "world");
         this.state.createPlayer(client.sessionId);
     }
 
-    onLeave (client) {
+    onLeave(client) {
         this.state.removePlayer(client.sessionId);
     }
 
-    onDispose () {
+    onDispose() {
         console.log("Dispose StateHandlerRoom");
     }
-
 }
